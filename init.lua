f7e009de8c64c2b7eb7a23df2ccdd8eaf466d14b
Marcin Musiał <marcin@everydayiplay.net>
2024-12-03 13:23:20 +0100

    add some custom params for gopls, to enable completeUnimported and use of placeholders

--- init.lua
+++ init.lua
@@ -544,7 +544,14 @@ require('lazy').setup({
         clangd = {
           filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'hpp' },
         },
-        gopls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              completeUnimported = true,
+              usePlaceholders = true,
+            },
+          },
+        },
         pyright = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
